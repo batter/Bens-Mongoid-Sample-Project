cf8e74f1179ca563373499166eceb76138af7db9
Ben Atkins <benatkins@fullbridge.com>
2011-10-05 16:02:23 -0400

    Removing unnecessary call to Mongoid.load, since Rails is smart enough to initialize mongoid.yml automatically.

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,6 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module MSampleApp
   class Application < Rails::Application
-    #load the DB
-    Mongoid.load!("config/mongoid.yml")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
